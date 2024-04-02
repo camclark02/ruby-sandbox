7040e5301fdf3ccc845b4024c9cc4956f8c15a14
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-02 15:12:35 +0000

    install gem

---
+++ howdy.rb
@@ -0,0 +1,21 @@
+require "date"
+# write your program here
+require "date"
+
+current_date = Date.today
+year = current_date.year
+day = current_date.day
+month = current_date.month
+
+pp "The year is: #{year}, the calendar day is: #{day}, and the month is: #{month}."
+
+number = 6
+another_number = 6.6
+a_string = "my numbers are"
+message = "today #{a_string}: #{number} and #{another_number}."
+pp message
+# require exercise
+my_string = "Hello, world!"
+pp my_string
+require "./goodbye.rb"
+require "active_support/all"
